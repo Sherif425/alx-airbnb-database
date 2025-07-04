38c3d8df3cf41cfc8923c4adb1e54b8fce8c7ac4
Sherif Mohamed <sherif425@gmail.com>
2025-07-04 17:38:41 +0300

    Update subqueries.sql
    Addung a correlated subquery to find users who have made more than 3 bookings.

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,3 +1,4 @@
+-- Write a query to find all properties where the average rating is greater than 4.0 using a subquery
 SELECT 
     p.property_id,
     p.name,
@@ -11,3 +12,16 @@ WHERE p.property_id IN (
     GROUP BY r.property_id
     HAVING AVG(r.rating) > 4.0
 );
+
+-- Write a correlated subquery to find users who have made more than 3 bookings.
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email
+FROM Users u
+WHERE (
+    SELECT COUNT(*) 
+    FROM Booking b 
+    WHERE b.user_id = u.user_id
+) > 3
