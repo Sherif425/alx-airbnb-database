f3b75d5847eefa0903dd608847517162d3b833e5
Sherif Mohamed <sherif425@gmail.com>
2025-07-04 17:42:12 +0300

    Update aggregations_and_window_functions.sql
    adding a query to find the total number of bookings made by each user, using the COUNT function and GROUP BY clause.

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,1 +1,10 @@
-
+-- Write a query to find the total number of bookings made by each user, using the COUNT function and GROUP BY clause.
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    COUNT(b.booking_id) AS total_bookings
+FROM Users u
+LEFT JOIN Booking b ON u.user_id = b.user_id
+GROUP BY u.user_id, u.first_name, u.last_name, u.email;
