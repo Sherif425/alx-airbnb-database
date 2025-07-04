0feb147b293b7fd3decd45e6ff285d148b234395
Sherif Mohamed <sherif425@gmail.com>
2025-07-04 17:32:46 +0300

    Update subqueries.sql
    Write a query to find all properties where the average rating is greater than 4.0 using a subquery.

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,13 @@
-
+SELECT 
+    p.property_id,
+    p.name,
+    p.description,
+    p.location,
+    p.pricepernight
+FROM Property p
+WHERE p.property_id IN (
+    SELECT r.property_id
+    FROM Review r
+    GROUP BY r.property_id
+    HAVING AVG(r.rating) > 4.0
+);
