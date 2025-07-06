fcac70dc0fbdc0a6280a2c24e9e5f62b37157838
Sherif Mohamed <sherif425@gmail.com>
2025-07-06 11:31:00 +0300

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,1 +1,5 @@
+-- Creating index on Booking.user_id for JOIN and WHERE clauses
+CREATE INDEX idx_booking_user_id ON Booking(user_id);
 
+-- Creating composite index on Property for GROUP BY columns
+CREATE INDEX idx_property_group_by ON Property(name, location, pricepernight);
